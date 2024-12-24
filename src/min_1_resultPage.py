895bd6e22fa828ed8d65355b7421e74ee2bca3e5
PTHoang123 <hoangphamlol456@gmail.com>
2024-12-24 15:49:36 +0700

    Revert "update level"
    This reverts commit 51f3a1c6c909de13908a87c894be5a1c6a1fd220.

--- src/min_1_resultPage.py
+++ src/min_1_resultPage.py
@@ -1,21 +1,6 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 import min_1_typingTest
 import mainMenu
-import os
-
-def read_all_text_files(directory):
-    text_files = [f for f in os.listdir(directory) if f.endswith('.txt')]
-    text_contents = {}
-
-    for file_name in text_files:
-        file_path = os.path.join(directory, file_name)
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text_contents[file_name] = file.read()
-
-    return text_contents
-
-directory = '/Users/mac/PythonFinalProject/Python-Final-Project/assests/topic_text'
-all_texts = read_all_text_files(directory)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -26,79 +11,55 @@ class Ui_MainWindow(object):
         MainWindow.setStyleSheet("background-color: #B1B2FF;")
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        
         self.heading_container = QtWidgets.QWidget(parent=self.centralwidget)
         self.heading_container.setGeometry(QtCore.QRect(0, 0, 1337, 56))
         self.heading_container.setMaximumSize(QtCore.QSize(1337, 56))
         self.heading_container.setStyleSheet("background-color: #AAC4FF;")
         self.heading_container.setObjectName("heading_container")
-        
         self.heading_label = QtWidgets.QLabel(parent=self.heading_container)
         self.heading_label.setGeometry(QtCore.QRect(621, 7, 91, 41))
+        self.heading_label.setStyleSheet("color: white;\n"
+"font: 24pt \"Bahnschrift\";\n"
+"font-weight: bold;")
         self.heading_label.setObjectName("heading_label")
-        
         self.result_container = QtWidgets.QWidget(parent=self.centralwidget)
         self.result_container.setGeometry(QtCore.QRect(40, 156, 1261, 421))
         self.result_container.setStyleSheet("background-color: #EEF1FF;\n"
-                                            "border-radius: 10px;")
+"border-radius: 10px;")
         self.result_container.setObjectName("result_container")
-        
-        # Define buttons that go in result_container
-        self.hard_text_btn = QtWidgets.QPushButton(parent=self.result_container)
-        self.hard_text_btn.setGeometry(QtCore.QRect(300, 329, 171, 41))
-        self.hard_text_btn.setStyleSheet("background-color: #FFD966;\n"
-                                         "border-radius: 5px;\n"
-                                         "color: black;\n"
-                                         "font: 63 16pt \"Bahnschrift SemiBold\";")
-        self.hard_text_btn.setObjectName("hard_text_btn")
-        self.hard_text_btn.clicked.connect(self.click_hard_text)
-        
-        self.very_hard_text_btn = QtWidgets.QPushButton(parent=self.result_container)
-        self.very_hard_text_btn.setGeometry(QtCore.QRect(789, 329, 171, 41))
-        self.very_hard_text_btn.setStyleSheet("background-color: #FFD966;\n"
-                                             "border-radius: 5px;\n"
-                                             "color: black;\n"
-                                             "font: 63 16pt \"Bahnschrift SemiBold\";")
-        self.very_hard_text_btn.setObjectName("very_hard_text_btn")
-        self.very_hard_text_btn.clicked.connect(self.click_very_hard_text)
-        
         self.result_heading = QtWidgets.QLabel(parent=self.result_container)
         self.result_heading.setGeometry(QtCore.QRect(476, 14, 321, 46))
         self.result_heading.setStyleSheet("color: black;\n"
-                                          "font: 63 28pt \"Bahnschrift SemiBold SemiConden\";\n"
-                                          "font-weight: bold;")
+"font: 63 28pt \"Bahnschrift SemiBold SemiConden\";\n"
+"font-weight: bold;\n"
+"")
         self.result_heading.setObjectName("result_heading")
-        
         self.test_complete_label = QtWidgets.QLabel(parent=self.result_container)
         self.test_complete_label.setGeometry(QtCore.QRect(440, 74, 381, 61))
         self.test_complete_label.setStyleSheet("font: 25 20pt \"Bahnschrift\";\n"
-                                               "background: none;\n"
-                                               "color: black")
+"background: none;\n"
+"color: black")
         self.test_complete_label.setObjectName("test_complete_label")
-        
-        self.wpm_and_level_label = QtWidgets.QLabel(parent=self.result_container)
-        self.wpm_and_level_label.setGeometry(QtCore.QRect(363, 141, 531, 61))
-        self.wpm_and_level_label.setStyleSheet("font: 25 20pt \"Bahnschrift\";\n"
-                                              "background: none;\n"
-                                              "color: black")
-        self.wpm_and_level_label.setObjectName("wpm_and_level_label")
-        
+        self.wpm_and_accuracy_label = QtWidgets.QLabel(parent=self.result_container)
+        self.wpm_and_accuracy_label.setGeometry(QtCore.QRect(363, 141, 531, 61))
+        self.wpm_and_accuracy_label.setStyleSheet("font: 25 20pt \"Bahnschrift\";\n"
+"background: none;\n"
+"color: black")
+        self.wpm_and_accuracy_label.setObjectName("wpm_and_accuracy_label")
         self.try_again_btn = QtWidgets.QPushButton(parent=self.result_container)
         self.try_again_btn.setGeometry(QtCore.QRect(300, 269, 171, 41))
         self.try_again_btn.setStyleSheet("background-color: #FFD966;\n"
-                                         "border-radius: 5px;\n"
-                                         "color: black;\n"
-                                         "font: 63 16pt \"Bahnschrift SemiBold\";")
+"border-radius: 5px;\n"
+"color: black;\n"
+"font: 63 16pt \"Bahnschrift SemiBold\";")
         self.try_again_btn.setObjectName("try_again_btn")
-        
         self.main_menu_btn = QtWidgets.QPushButton(parent=self.result_container)
         self.main_menu_btn.setGeometry(QtCore.QRect(789, 269, 171, 41))
         self.main_menu_btn.setStyleSheet("background-color: #FFD966;\n"
-                                         "border-radius: 5px;\n"
-                                         "color: black;\n"
-                                         "font: 63 16pt \"Bahnschrift SemiBold\";")
+"border-radius: 5px;\n"
+"color: black;\n"
+"font: 63 16pt \"Bahnschrift SemiBold\";")
         self.main_menu_btn.setObjectName("main_menu_btn")
-        
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -108,18 +69,11 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
         # Connect buttons to functions
-        self.try_again_btn.clicked.connect(lambda: self.try_again(self.current_text))
+        self.try_again_btn.clicked.connect(self.try_again)
         self.main_menu_btn.clicked.connect(self.go_to_main_menu)
 
-    def display_results(self, wpm, level, suggested_texts, current_text):
-        self.suggested_texts = suggested_texts
-        self.current_text = current_text
-        self.wpm_and_level_label.setText(
-            f"<html><head/><body><p align=\"center\"><span style=\" font-weight:400;\">Your speed was </span>"
-            f"<span style=\" font-weight:400; color:#23d83e;\">{wpm:.2f} WPM</span>"
-            f"<span style=\" font-weight:400;\"> and your level is </span>"
-            f"<span style=\" font-weight:400; color:#23d83e;\">{level}</span></p></body></html>"
-        )
+    def display_results(self, wpm):
+        self.wpm_and_accuracy_label.setText(f"<html><head/><body><p align=\"center\"><span style=\" font-weight:400;\">Your speed was </span><span style=\" font-weight:400; color:#23d83e;\">{wpm:.2f} WPM</span></p></body></html>")
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -130,44 +84,10 @@ class Ui_MainWindow(object):
         self.try_again_btn.setText(_translate("MainWindow", "Try Again"))
         self.main_menu_btn.setText(_translate("MainWindow", "Main Menu"))
 
-    def click_hard_text(self):
-        if hasattr(self, 'suggested_texts') and len(self.suggested_texts) > 0:
-            selected_text = self.suggested_texts[0]
-            print(f"Hard Text Selected: {selected_text}")  # Debugging line
-            self.window = QtWidgets.QMainWindow()
-            self.ui = min_1_typingTest.Ui_MainWindow()
-            self.ui.setupUi(self.window)
-            self.ui.selected_text = selected_text
-            self.window.show()
-            self.MainWindow.close()
-        else:
-            QtWidgets.QMessageBox.warning(self.MainWindow, "Error", "No Hard Text available.")
-
-    def click_very_hard_text(self):
-        if hasattr(self, 'suggested_texts') and len(self.suggested_texts) > 1:
-            selected_text = self.suggested_texts[1]
-            print(f"Very Hard Text Selected: {selected_text}")  # Debugging line
-            self.window = QtWidgets.QMainWindow()
-            self.ui = min_1_typingTest.Ui_MainWindow()
-            self.ui.setupUi(self.window)
-            self.ui.selected_text = selected_text
-            self.window.show()
-            self.MainWindow.close()
-        else:
-            QtWidgets.QMessageBox.warning(self.MainWindow, "Error", "No Very Hard Text available.")
-
-    def try_again(self, current_text):
-        print(f"Trying again with text: {current_text}")  # Debugging line
+    def try_again(self):
         self.window = QtWidgets.QMainWindow()
         self.ui = min_1_typingTest.Ui_MainWindow()
         self.ui.setupUi(self.window)
-        if current_text:
-            # Reuse the same text for the next typing session
-            self.ui.selected_text = current_text
-            self.ui.text_area.setPlainText(current_text)
-        else:
-            self.ui.selected_text = None
-            self.ui.text_area.setPlainText(all_texts.get('book.txt', ''))
         self.window.show()
         self.MainWindow.close()  # Close the result window
 
@@ -184,10 +104,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    # Provide two suggested texts for testing
-    ui.display_results(80, "Expert", [
-        all_texts.get('what-screams-i-have-depression.txt', 'Default Hard Text'),
-        all_texts.get('very_hard_text.txt', 'Default Very Hard Text')
-    ], current_text=all_texts.get('book.txt', 'Default Book Text'))
+    ui.display_results(80)  # Example WPM
     MainWindow.show()
     sys.exit(app.exec())
--- src/min_1_typingTest.py
+++ src/min_1_typingTest.py
@@ -7,23 +7,8 @@
 
 
 from PyQt6 import QtCore, QtGui, QtWidgets
-import min_1_resultPage
 import mainMenu
-import os
-
-def read_all_text_files(directory):
-    text_files = [f for f in os.listdir(directory) if f.endswith('.txt')]
-    text_contents = {}
-
-    for file_name in text_files:
-        file_path = os.path.join(directory, file_name)
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text_contents[file_name] = file.read()
-
-    return text_contents
-
-directory = '/Users/mac/PythonFinalProject/Python-Final-Project/assests/topic_text'
-all_texts = read_all_text_files(directory)
+import min_1_resultPage
 
 class Ui_MainWindow(object):
     
@@ -33,21 +18,19 @@ class Ui_MainWindow(object):
         self.ui.setupUi(self.window)
         self.window.show()
         current_window.hide()
-    
+
     def setupUi(self, MainWindow):
         self.MainWindow = MainWindow  # Store the MainWindow reference
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1337, 720)
-        self.selected_text = None
         MainWindow.setMaximumSize(QtCore.QSize(1337, 720))
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        
         self.heading_background = QtWidgets.QWidget(parent=self.centralwidget)
         self.heading_background.setGeometry(QtCore.QRect(0, 0, 1337, 78))
-        self.heading_background.setStyleSheet("background-color: #AAC4FF;")
+        self.heading_background.setStyleSheet("background-color: #AAC4FF\n"
+"")
         self.heading_background.setObjectName("heading_background")
-        
         self.return_btn = QtWidgets.QPushButton(parent=self.heading_background)
         self.return_btn.setGeometry(QtCore.QRect(20, 20, 71, 41))
         self.return_btn.clicked.connect(lambda: self.back_to_main_menu(MainWindow))
@@ -71,7 +54,6 @@ class Ui_MainWindow(object):
             }
         """)
         self.return_btn.setObjectName("return_btn")
-        
         self.restart_btn = QtWidgets.QPushButton(parent=self.heading_background)
         self.restart_btn.setGeometry(QtCore.QRect(1250, 20, 71, 41))
         self.restart_btn.setStyleSheet("""
@@ -94,53 +76,39 @@ class Ui_MainWindow(object):
             }
         """)
         self.restart_btn.setObjectName("restart_btn")
-        self.restart_btn.clicked.connect(self.restart_test)
+        self.restart_btn.clicked.connect(self.replay)
         self.heading_label = QtWidgets.QLabel(parent=self.heading_background)
         self.heading_label.setGeometry(QtCore.QRect(514, 0, 400, 75))
         self.heading_label.setObjectName("heading_label")
-        
         self.label = QtWidgets.QLabel(parent=self.heading_background)
         self.label.setGeometry(QtCore.QRect(1156, 0, 61, 78))
         self.label.setStyleSheet("color: white;\n"
-                                 "font: 63 24pt \"Bahnschrift SemiBold SemiConden\";")
+"font: 63 24pt \"Bahnschrift SemiBold SemiConden\";\n"
+"")
         self.label.setObjectName("label")
-        
         self.background = QtWidgets.QWidget(parent=self.centralwidget)
         self.background.setGeometry(QtCore.QRect(0, 78, 1337, 642))
-        self.background.setStyleSheet("background-color: #B1B2FF;")
+        self.background.setStyleSheet("background-color: #B1B2FF;\n"
+"")
         self.background.setObjectName("background")
-        
         self.text_area_background = QtWidgets.QWidget(parent=self.background)
         self.text_area_background.setGeometry(QtCore.QRect(40, 69, 1261, 181))
         self.text_area_background.setStyleSheet("font-size: 48pt;\n"
-                                               "background-color: #fff;\n"
-                                               "border-radius: 5px;")
+"background-color: #fff;\n"
+"border-radius: 5px;\n"
+"")
         self.text_area_background.setObjectName("text_area_background")
-        
         self.text_area = QtWidgets.QTextEdit(parent=self.text_area_background)
         self.text_area.setGeometry(QtCore.QRect(10, 10, 1231, 171))
         self.text_area.setStyleSheet("font-size: 48pt;\n"
 "contenteditable=\"false\";")
         self.text_area.setObjectName("text_area")
-        self.typing_area_background_2 = QtWidgets.QWidget(parent=self.background)
-        self.typing_area_background_2.setGeometry(QtCore.QRect(40, 310, 1261, 181))
-        self.typing_area_background_2.setStyleSheet("font-size: 48pt;\n"
-"background-color: #fff;\n"
-"border-radius: 5px;\n"
-"")
-        self.typing_area_background_2.setObjectName("typing_area_background_2")
-        self.typing_area = QtWidgets.QTextEdit(parent=self.typing_area_background_2)
-        self.typing_area.setGeometry(QtCore.QRect(10, 10, 1231, 171))
-        self.typing_area.setStyleSheet("font-size: 22pt;")
-        self.typing_area.setObjectName("typing_area")
-        self.typing_area.textChanged.connect(self.check_typing)
-        
+        self.text_area.textChanged.connect(self.check_typing)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1337, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        
         self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -160,39 +128,13 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "Minute Typing Test"))
         self.return_btn.setText(_translate("MainWindow", "←"))
         self.restart_btn.setText(_translate("MainWindow", "⟳"))
-        self.heading_label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-family:'Bahnschrift'; font-size:28pt; font-weight:600; color:#ffffff;\">1 Minute typing test</span></p></body></html>"))
-        self.heading_label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-family:'Bahnschrift'; font-size:28pt; font-weight:600; color:#ffffff;\">1 Minute typing test</span></p></body></html>"))
+        self.heading_label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-family:\'Bahnschrift\'; font-size:28pt; font-weight:600; color:#ffffff;\">1 Minute typing test</span></p></body></html>"))
         self.label.setText(_translate("MainWindow", "1:00"))
         self.text_area.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:48pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Bahnschrift'; font-size:22pt; font-weight:600; color:#000000;\">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,  ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate  eget, </span></p></body></html>"))
-        self.typing_area.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:22pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"justify\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
-
-    def check_typing(self):
-        expected_text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,"
-        user_text = self.typing_area.toPlainText()
-        formatted_text = ""
-
-        for i, char in enumerate(user_text):
-            if i < len(expected_text) and char == expected_text[i]:
-                formatted_text += f'<span style="color:#23d83e;">{char}</span>'
-            else:
-                formatted_text += f'<span style="color:#ea0c0f;">{char}</span>'
-
-        cursor = self.typing_area.textCursor()
-        cursor_position = cursor.position()
-
-        self.typing_area.blockSignals(True)
-        self.typing_area.setHtml(formatted_text)
-        cursor.setPosition(cursor_position)
-        self.typing_area.setTextCursor(cursor)
-        self.typing_area.blockSignals(False)
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:48pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#23d83e;\">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo </span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#ea0c0f;\">li</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#23d83e;\">gula </span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#ea0c0f;\">ege</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#23d83e;\">t dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nas</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#ea0c0f;\">c</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#23d83e;\">etur ridiculus mus. Donec quam felis,  ultricies nec, pellentesque eu, pretium q</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#ea0c0f;\">ui</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#23d83e;\">s, sem. Nulla consequat massa quis enim. Donec pede justo, fring</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#ea0c0f;\">i</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600; color:#23d83e;\">lla vel, aliquet nec, vulputate  eget,</span><span style=\" font-family:\'Bahnschrift\'; font-size:24pt; font-weight:600;\"> </span></p></body></html>"))
 
     def update_timer(self):
         if self.time_left > 0:
@@ -205,44 +147,45 @@ class Ui_MainWindow(object):
             self.typing_area.setReadOnly(True)
             self.calculate_wpm()
 
+    def check_typing(self):
+        expected_text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,"
+        user_text = self.text_area.toPlainText()
+        formatted_text = ""
+    
+        for i, char in enumerate(user_text):
+            if i < len(expected_text) and char == expected_text[i]:
+                formatted_text += f'<span style="color:#23d83e;">{char}</span>'
+            else:
+                formatted_text += f'<span style="color:#ea0c0f;">{char}</span>'
+    
+        cursor = self.text_area.textCursor()
+        cursor_position = cursor.position()
+    
+        self.text_area.blockSignals(True)
+        self.text_area.setHtml(formatted_text)
+        cursor.setPosition(cursor_position)
+        self.text_area.setTextCursor(cursor)
+        self.text_area.blockSignals(False)
+
     def calculate_wpm(self):
-        user_text = self.typing_area.toPlainText()
+        user_text = self.text_area.toPlainText()
         num_chars = len(user_text)
-        wpm = num_chars / 5  # Calculate WPM based on the number of characters typed
+        wpm = num_chars / 5  # Since the test is 1 minute, we don't need to divide by time
         self.open_result_page(wpm)
-        
     def open_result_page(self, wpm):
         self.window = QtWidgets.QMainWindow()
         self.ui = min_1_resultPage.Ui_MainWindow()
         self.ui.setupUi(self.window)
-
-        # Decide suggested texts
-        suggested_texts = []
-        if level in ["Advanced", "Expert"]:
-            suggestion_msg = "You did great! Try a harder text next time."
-            suggested_texts = [
-                all_texts.get('what-screams-i-have-depression.txt', 'Default Hard Text'),
-                all_texts.get('very_hard_text.txt', 'Default Very Hard Text')
-            ]
-        else:
-            suggestion_msg = "Keep practicing! Try an easier text to improve your skills."
-            suggested_texts = [
-                all_texts.get('book.txt', 'Default Book Text'),
-                all_texts.get('cats.txt', 'Default Cats Text')
-            ]
-        self.ui.display_results(wpm, level, suggested_texts, current_text)
-        # Override the "Try Again" button in result page to reset to default text
-        self.ui.try_again_btn.clicked.disconnect()
-        self.ui.try_again_btn.clicked.connect(lambda: self.ui.try_again(current_text))
+        self.ui.display_results(wpm)
         self.window.show()
-        self.MainWindow.close()  # Use the stored MainWindow reference
-    
-    def restart_test(self):
-        self.time_left = 60  # Reset timer to 1 minute
-        self.label.setText("1:00")
-        self.typing_area.setReadOnly(False)
-        self.typing_area.clear()
+        self.MainWindow.close()  
+    def replay(self):
+        self.time_left = 60  # Reset the timer to 1 minute
+        self.label.setText("1:00")  # Reset the label
+        self.text_area.setReadOnly(False)  # Make the text area editable
+        self.text_area.clear()  # Clear the text area
         self.timer.start(1000)  # Restart the timer
+
 
 if __name__ == "__main__":
     import sys
